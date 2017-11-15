1d3aa5d7230e2f1a7d9c216d45b62c7777a33f36
taleminh <stefanleminh@gmail.com>
2017-11-15 15:27:14 +0100

    Add form to javascript, add click handler for figures

--- public/javascripts/chess.js
+++ public/javascripts/chess.js
@@ -4,4 +4,9 @@ function registerClick(){
 
 $( document ).ready(function() {
     console.log( "Document is ready" );
+    $('.img-responsive').click(function(){
+        console.log("Ich wurde geklickt!");
+    });
 });
+
+
