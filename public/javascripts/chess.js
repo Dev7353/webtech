2b0f14b293551a50981497baa108686e4022e888
taleminh <stefanleminh@gmail.com>
2017-11-22 13:40:47 +0100

    Add x and y coordinates to tiles and figures, prototypes for ajax calls

--- public/javascripts/chess.js
+++ public/javascripts/chess.js
@@ -4,16 +4,29 @@ function submitform() {
     window.location.href = "/login/:" + name1 + ":" + name2;
 }
 function sendData(coordinate, player, count) {
-    $.ajax({
-        type: "GET",
-        url: "/getMoves/:x:y:p:c"
-    });
+    // $.ajax({
+    //     type: "GET",
+    //     url: "/getMoves/:x:y:p:c"
+    // });
 }
 
 $( document ).ready(function() {
+    let count = 0;
     console.log( "Document is ready" );
     $('.img-responsive').click(function(){
-        console.log(this.parentElement);
+        count++;
+        console.log("Count wurde inkrementiert!")
+    });
+    $('.tile').click(function(){
+        if(count === 1){
+            //sendData
+            console.log("Count ist 1!")
+            //If coordinate is in received JSON, set Figure
+            count = 0;
+            console.log("Count wurde dekrementiert!");
+        } else {
+            console.log("Count ist 0!")
+        }
     });
 });
 
