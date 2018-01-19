cb26fd6b8cbc0b189c2a66dfd63913d43710cd4a
taleminh <stefanleminh@gmail.com>
2018-01-19 16:37:49 +0100

    Revert sessionsocket changes

--- public/javascripts/session.js
+++ public/javascripts/session.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 })
 
 function connectSessionSocket() {
-    socket = new WebSocket("wss://de-chess-htwg.herokuapp.com/SessionSocket");
+    socket = new WebSocket("ws://localhost:9000/SessionSocket");
     socket.onopen = function () {
         console.log("Open to connect SessionSocket ...");
         socket.send(playername)
@@ -15,7 +15,7 @@ function connectSessionSocket() {
     socket.onmessage = function (e) {
         if (typeof e.data === "string" && e.data === "READY") {
             //.. start game
-            window.location.replace("http://de-chess-htwg.herokuapp.com/game");
+            window.location.replace("http://localhost:9000/game");
         } else if(typeof e.data === "string" && e.data === "WAIT"){
             console.log("waiting ...") //TODO: Give feedback to user
         }
