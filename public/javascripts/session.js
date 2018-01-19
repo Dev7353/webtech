0d863758c5d30a0c58e4300b2d34a51c5765effc
Stefan <stefanleminh@gmail.com>
2018-01-20 00:50:42 +0100

    Rework style, show rules after sign in, add animations, add text when waiting in queue

--- public/javascripts/session.js
+++ public/javascripts/session.js
@@ -1,8 +1,12 @@
 var playername
 $(document).ready(function(){
+    window.sr = ScrollReveal();
+    sr.reveal(".ruleset");
     playername = $('#fullname').text()
     $('#start').click(function(){
-        connectSessionSocket()
+        $('#queueText').css("visibility", "visible");
+        $('#queueText').fadeTo(300, 1)
+        connectSessionSocket();
     })
 })
 
@@ -29,3 +33,31 @@ function connectSessionSocket() {
 
 
 }
+// Animations
+// Wrap every letter in a span
+$('.ml1 .letters').each(function(){
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: false})
+    .add({
+        targets: '.ml1 .letter',
+        scale: [0.3,1],
+        opacity: [0,1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 600,
+        delay: function(el, i) {
+            return 70 * (i+1)
+        }
+    }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: function(el, i, l) {
+        return 80 * (l - i);
+    }
+});
